d357631bf6bc0fdc0fbef58aff74777002583929
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2019-09-28 17:59:15 -0300

    Fix run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,8 @@
 
 BASE=$(cd "$(dirname $0)/../"; pwd)
 RUNTIME="--runtime=nvidia"
-if [ "$1" == "cpu"]; then
+if [ "$1" == "cpu" ]; then
 	RUNTIME=''
+	shift 1
 fi
 sudo docker run -it $RUNTIME -v $BASE:/lpcnet lpcnet-train:1.0.0 $@
